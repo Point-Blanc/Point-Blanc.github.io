db4199ad81194e0b6b8a21ca1491d18fb51a1f7f
IceCereal <ns_raghav@hotmail.com>
2019-10-03 19:25:05 +0530

    update timer code

--- assets/js/countdown-timer.js
+++ assets/js/countdown-timer.js
@@ -1,35 +1,34 @@
 function updateCountdownTime() {
 	now = new Date();
-	kickoff = Date.parse("November 9, 2019 10:30:10");
+	start_PB = Date.parse("November 9, 2009 10:30:10");
+	end_PB = Date.parse("November 10, 2019 11:30:10");
 
-	diff = kickoff - now;
+	diff_start = start_PB - now;
+	diff_end = end_PB - now;
 
-	if (diff < 0){
+	if (diff_start < 0 && diff_end > 0){
 		document.getElementById("countdown-timer").hidden = true;
 		document.getElementById("countdown-timer-completed").hidden = false;
-	}
-	else {
-		days = Math.floor( diff / (1000*60*60*24) );
-		hours = Math.floor( diff / (1000*60*60) );
-		mins = Math.floor( diff / (1000*60) );
-		secs = Math.floor( diff / 1000 );
+		document.getElementById("countdown-timer-completed").innerHTML = "PointBlanc is happening <b style=\"font-size: 0.9em;\">right now</b>. ─=≡Σ(╯°□°)╯︵┻┻ ";
+	} else if (diff_start < 0 && diff_end < 0){
+		document.getElementById("countdown-timer").hidden = true;
+		document.getElementById("countdown-timer-completed").hidden = false;
+		document.getElementById("countdown-timer-completed").innerHTML = "PointBlanc is done. (٥↼_↼)";
+	} else{
+		days = Math.floor( diff_start / (1000*60*60*24) );
+		hours = Math.floor( diff_start / (1000*60*60) );
+		mins = Math.floor( diff_start / (1000*60) );
+		secs = Math.floor( diff_start / 1000 );
 
 		dd = days;
 		hh = hours - days * 24;
 		mm = mins - hours * 60;
 		ss = secs - mins * 60;
 
-		document.getElementById("countdown-timer-days").innerHTML =
-			dd;
-		document.getElementById("countdown-timer-hours").innerHTML =
-			hh;
-		document.getElementById("countdown-timer-minutes").innerHTML =
-			mm;
-		document.getElementById("countdown-timer-seconds").innerHTML =
-			ss;
-		// document.getElementById("countdown-timer-completed").hidden = true;
-		
+		document.getElementById("countdown-timer-days").innerHTML = dd;
+		document.getElementById("countdown-timer-hours").innerHTML = hh;
+		document.getElementById("countdown-timer-minutes").innerHTML = mm;
+		document.getElementById("countdown-timer-seconds").innerHTML = ss;
 	}
-
 }
-setInterval('updateCountdownTime()', 1000 );
+setInterval('updateCountdownTime()', 1000);
